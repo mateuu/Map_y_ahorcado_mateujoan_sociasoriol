daaa6de6b7d2c4b44a56ed7ad520ae5ed9d10284
mateuu <74050219+mateuu@users.noreply.github.com>
2024-11-03 19:06:47 +0100

    mapa actualizado

--- socias_oriol_mateu_joan/socias_oriol_mateu_joan/socias_oriol_mateu_joan.cpp
+++ socias_oriol_mateu_joan/socias_oriol_mateu_joan/socias_oriol_mateu_joan.cpp
@@ -5,4 +5,14 @@
 int main() {
 	char map[10][10];
 	int pX = 0, pY = 0;
+
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+
+			map[i][j] = '-';
+		}
+	}
+	map[pX][pY] = 'X';
+	map[4][4] = 'R';
 }
