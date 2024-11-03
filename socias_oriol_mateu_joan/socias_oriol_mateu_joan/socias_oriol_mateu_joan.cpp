a4baacb020d6280fe7ae15af4724f626e8c88645
mateuu <74050219+mateuu@users.noreply.github.com>
2024-11-03 19:36:18 +0100

    Penjat principi

--- socias_oriol_mateu_joan/socias_oriol_mateu_joan/socias_oriol_mateu_joan.cpp
+++ socias_oriol_mateu_joan/socias_oriol_mateu_joan/socias_oriol_mateu_joan.cpp
@@ -7,7 +7,10 @@ int main()
 	char map[10][10];
 	int pX = 0, pY = 0;
 	int vides = 3;
-
+	int lletracorrecte = 0;
+	char paraula[] = "excelent";
+	int longparaula = 6;
+	char estatparaula[7] = "";
 
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++) {
@@ -38,4 +41,44 @@ int main()
 	scanf_s("%d", &accio);
 
 	map[pX][pY] = '-';
+	switch (accio)
+	{
+	case 1:
+		if (pX > 0)
+		{
+			pX--;
+		}
+	case 2:
+		if (pX < 9)
+		{
+			pX++;
+		}
+	case 3:
+		if (pY > 0)
+		{
+			pY--;
+		}
+	case 4:
+		if (pY < 9)
+		{
+			pY++;
+		}
+	default:
+		printf("Accio no valida");
+}
+	map[pX][pY] = 'X';
+
+	if (pX == 4 && pY == 4)
+	{
+		printf("has trobat el repte del penjat! \n");
+		while (vides > 0 && lletracorrecte < longparaula) {
+		
+			printf("Paraula: ");
+			for (int i = 0; i < longparaula; i++)
+			{
+				printf("%c", estatparaula[i]);
+			}
+			printf("\n");
+		}
+	}
 }
